89c63c29926c71abcf3c6cc8269b69a693f1b643
Jose Urbaneja <tucorreo@ejemplo.com>
2025-08-05 16:52:47 -0400

    deposito

--- templates/Users/niveles_inversion.php
+++ templates/Users/niveles_inversion.php
@@ -6,7 +6,7 @@ $this->assign('title', 'Niveles de Inversión');
 
    <div class="level">Nivel 1 – Inversión mínima: 10 USDT</div>
     <div class="benefit">⏳ Tiempo: 20 días</div>
-    <div class="benefit">💰 Ganancia diaria: 18%</div>
+    <div class="benefit">💰 Ganancia diaria: 16%</div>
 </div>
 
 
@@ -15,7 +15,7 @@ $this->assign('title', 'Niveles de Inversión');
 
      <div class="level">Nivel 2 – Inversión mínima: 50 USDT</div>
     <div class="benefit">⏳ Tiempo: 20 días</div>
-    <div class="benefit">💰 Ganancia diaria: 20%</div>
+    <div class="benefit">💰 Ganancia diaria: 18%</div>
 </div>
 
 
@@ -23,14 +23,14 @@ $this->assign('title', 'Niveles de Inversión');
 
         <div class="level">Nivel 3 – Inversión mínima: 200 USDT</div>
     <div class="benefit">⏳ Tiempo: 20 días</div>
-    <div class="benefit">💰 Ganancia diaria: 21%</div>
+    <div class="benefit">💰 Ganancia diaria: 20%</div>
 </div>
 
 <div class="bg-[#1E1E1E] text-white rounded-xl p-5 shadow max-w-md mx-auto mt-6 justify-center text-center">
 
      <div class="level">Nivel 4 – Inversión mínima: 500 USDT</div>
     <div class="benefit">⏳ Tiempo: 20 días</div>
-    <div class="benefit">💰 Ganancia diaria: 22%</div>
+    <div class="benefit">💰 Ganancia diaria: 21%</div>
 </div>
 
 
